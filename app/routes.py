7f84059f2ea9fef14ff5b8882827a128ef1a7cf3
Sara Gorecki <sgorecki@gmail.com>
2020-04-01 16:11:18 -0400

    Add initial template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    text = {'text': 'Go away coronavirus'}
+    return render_template('index.html', title='Home', text=text)
